fdf5ce6f4ab24d7dbe1b9da5293b595876ac0958
nsrivastavaFiserv <srivastava.nishant1991@gmail.com>
2019-04-06 22:26:07 +0530

    changes done for travis first go

--- src/main/java/com/test/tracker/TrackerIndexController.java
+++ src/main/java/com/test/tracker/TrackerIndexController.java
@@ -1,13 +1,22 @@
 package com.test.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TrackerIndexController {
 
-    @GetMapping("/")
+	
+	private String appOwnerName;
+	
+    public TrackerIndexController( @Value("${appowner.name}")String appOwnerName) {
+		super();
+		this.appOwnerName = appOwnerName;
+	}
+
+	@GetMapping("/")
     public String showIndexMessage(){
-        return "This is Index Page";
+        return "This is Index Page for app owner >>>"+appOwnerName;
     }
 }
